670d79e492a4bc16d387648f85a27b81ca13b762
sahhhall <sahalvavoor313@gmail.com>
2024-02-19 14:23:18 +0530

    void

---
+++ src/voidType.ts
@@ -0,0 +1,8 @@
+// the  voide type denotes the absesnce having any type at all, its juust opposist of any 
+
+// typically  use the void type as the funtion return type that do not return a value 
+
+const sum = (m:string): void  =>{
+    console.log(m);
+    
+}
